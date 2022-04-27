a2492f248cfe10fce31087a66dd4445ea3bc179f
liwei <1936956039@qq.com>
2022-04-27 17:23:47 +0800

    hot-fix first commit

--- src/test/java/com/liwei/git/test/Test.java
+++ src/test/java/com/liwei/git/test/Test.java
@@ -4,5 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("新增一条");
+        System.out.println("再新增一条");
+        System.out.println("再次新增一条");
     }
 }
